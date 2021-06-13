c8f59b21ef4bbcfae09421f415392a6af53e80e5
Ashley Porter <porter.ashley13@gmail.com>
2021-06-13 00:33:37 -0700

    Updates

---
+++ precache-manifest.f185e234c187deb063a444ce127480da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5469c28a45556446d25e307ded92543c",
+    "url": "/outage-tracker-deployable/index.html"
+  },
+  {
+    "revision": "cc74485959e9a8c08374",
+    "url": "/outage-tracker-deployable/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "fd21036a036a7008a80f",
+    "url": "/outage-tracker-deployable/static/js/2.cf95128e.chunk.js"
+  },
+  {
+    "revision": "df95b0e411069ea254bd",
+    "url": "/outage-tracker-deployable/static/js/3.7b872907.chunk.js"
+  },
+  {
+    "revision": "cc74485959e9a8c08374",
+    "url": "/outage-tracker-deployable/static/js/main.2bdd25ec.chunk.js"
+  },
+  {
+    "revision": "37aeb5289d18937d4475",
+    "url": "/outage-tracker-deployable/static/js/runtime-main.fedda207.js"
+  }
+]);
